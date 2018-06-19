6392e6f5b7896a9704d71bc47dad2b0b2eb4754a
Unknown <colber16@gmail.com>
2018-06-18 21:50:40 -0400

    Added getters for time properties.

--- KimTower/KimTower.UnitTests/TimeTests.cs
+++ KimTower/KimTower.UnitTests/TimeTests.cs
@@ -9,7 +9,7 @@ namespace KimTower.UnitTests
         [TestCase(1, 2, Day.WeekdayOne, 12, 30)]
         [TestCase(2, 1, Day.WeekdayTwo, 6, 0)]
         [TestCase(4, 3, Day.Weekend, 18, 45)]
-        [TestCase(10, 4, Day.WeekdayOne, 0, 0)]
+        [TestCase(10, 0, Day.WeekdayOne, 0, 0)]
         public void MinuteGetter(int year, int quarter, Day day, int hour, int minute)
         {
             var time = new Time(year, quarter, day, hour, minute);
@@ -21,7 +21,7 @@ namespace KimTower.UnitTests
         [TestCase(1, 2, Day.WeekdayOne, 12, 30)]
         [TestCase(2, 1, Day.WeekdayTwo, 6, 0)]
         [TestCase(4, 3, Day.Weekend, 18, 45)]
-        [TestCase(10, 4, Day.WeekdayOne, 0, 0)]
+        [TestCase(10, 0, Day.WeekdayOne, 0, 0)]
         public void HourGetter(int year, int quarter, Day day, int hour, int minute)
         {
             var time = new Time(year, quarter, day, hour, minute);
@@ -33,7 +33,7 @@ namespace KimTower.UnitTests
         [TestCase(1, 2, Day.WeekdayOne, 12, 30)]
         [TestCase(2, 1, Day.WeekdayTwo, 6, 0)]
         [TestCase(4, 3, Day.Weekend, 18, 45)]
-        [TestCase(10, 4, Day.WeekdayOne, 0, 0)]
+        [TestCase(10, 0, Day.WeekdayOne, 0, 0)]
         public void DayGetter(int year, int quarter, Day day, int hour, int minute)
         {
             var time = new Time(year, quarter, day, hour, minute);
@@ -45,7 +45,7 @@ namespace KimTower.UnitTests
         [TestCase(1, 2, Day.WeekdayOne, 12, 30)]
         [TestCase(2, 1, Day.WeekdayTwo, 6, 0)]
         [TestCase(4, 3, Day.Weekend, 18, 45)]
-        [TestCase(10, 4, Day.WeekdayOne, 0, 0)]
+        [TestCase(10, 0, Day.WeekdayOne, 0, 0)]
         public void QuarterGetter(int year, int quarter, Day day, int hour, int minute)
         {
             var time = new Time(year, quarter, day, hour, minute);
@@ -57,7 +57,7 @@ namespace KimTower.UnitTests
         [TestCase(1, 2, Day.WeekdayOne, 12, 30)]
         [TestCase(2, 1, Day.WeekdayTwo, 6, 0)]
         [TestCase(4, 3, Day.Weekend, 18, 45)]
-        [TestCase(10, 4, Day.WeekdayOne, 0, 0)]
+        [TestCase(10, 0, Day.WeekdayOne, 0, 0)]
         public void YearGetter(int year, int quarter, Day day, int hour, int minute)
         {
             var time = new Time(year, quarter, day, hour, minute);
--- KimTower/KimTower/Time.cs
+++ KimTower/KimTower/Time.cs
@@ -15,7 +15,7 @@ namespace KimTower
 
         public Time(int year, int quarter, Day day, int hour, int minute)
         {
-            this.TotalMinutes = minute + hour * 60 ;
+            this.TotalMinutes = minute + (hour * 60) + (quarter * 3 * 24 * 60) + GetMinutesFromDay(day) + (year * 4 * 3 * 24 * 60);
         }
 
         public int Minute { get { return TotalMinutes % 60; } }
@@ -24,12 +24,45 @@ namespace KimTower
 
         public int Hour { get { return (TotalMinutes / 60) % 24; } }
 
-        public Day Day { get;}
+        public Day Day { get { return GetDayFromMinutes(TotalMinutes); } }
 
-        public int Quarter { get { return (TotalMinutes / 60) % (24 * 3); } }
+        public int Quarter { get { return (TotalMinutes / (24 * 60 * 3) % 4); } }
+       
+        public int Year { get { return (TotalMinutes / (24 * 60 * 3 * 4)); } }
 
-        public int Year { get { return (TotalMinutes / 60) % (24 * 3 * 4); } }
-        
+        public Day GetDayFromMinutes(int minutes)
+        {
+            var day = (minutes / (24 * 60) % 3);
+
+            if(day == 0)
+            {
+                return Day.WeekdayOne;
+            }
+            if(day == 1)
+            {
+                return Day.WeekdayTwo;
+            }
+            else
+            {
+                return Day.Weekend;
+            }
+        }
+
+        public int GetMinutesFromDay(Day day)
+        {
+            if(day == Day.WeekdayOne)
+            {
+                return 0;
+            }
+            if (day == Day.WeekdayTwo)
+            {
+                return 24 * 60;
+            }
+            else
+            {
+                return 48 * 60;
+            }
+        }
 
     }
 
