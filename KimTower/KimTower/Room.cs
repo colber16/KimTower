e0344cab11580c1320cfb81c7dc15d3fd7def994
Unknown <colber16@gmail.com>
2018-06-30 19:43:54 -0400

    Added GetRange();

--- KimTower/KimTower/Room.cs
+++ KimTower/KimTower/Room.cs
@@ -21,11 +21,11 @@ namespace KimTower
         /// <value>The population.</value>
         int Population { get; set; }
 
-        int IsLit { get; set; }
+        bool IsLit { get; set; }
 
         //int ConstructionCost { get; set; }
 
-        Range Range { get; set; }
+        Range Range { get;  }
 
         int FloorNumber { get; set; }
 
@@ -35,27 +35,31 @@ namespace KimTower
        
 
     }
-    /// <inheritdoc/>
+
     public class Office : IRoom
     {
         private int segments = 9;
         private int capacity = 6;
+        private int xSecondCooridnate = 0;
        
-        public bool Occupied { get; set; }
+        public bool Occupied { get; set; } // is there transportation.  Are conditions good?
         public int Capacity => capacity;
-        public int Population { get; set; } // set by time?
-        public int IsLit { get; set; } //set by time
-        public Range Range { get; set; }
+        public int Population { get; set; } // set by time
+        public bool IsLit { get; set; } //set by time
+        public Range Range { get; }
         public int FloorNumber { get; set; }
 
         public int Segements => segments;
 
-        public Office(Floor floor, Range range)
+        public Office(Floor floor)
         {
             this.FloorNumber = floor.FloorNumber;
-            this.Range = range;
+            this.Range = GetRange();
 
         }
+
+        private Range GetRange() => new Range(this.xSecondCooridnate, this.xSecondCooridnate + segments);
+
     }
 
 }
