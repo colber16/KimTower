ad0bfb2390b3a7dd369db7863f8c3790d5d8b841
Kia Colbert <colber16@gmail.com>
2018-07-27 19:03:33 -0400

    Tower money, mayber?

--- Data/Tower.cs
+++ Data/Tower.cs
@@ -8,11 +8,22 @@ namespace KimTower.Data
     {
         public List<Floor> Floors { get; set; }
 
+        public Ledger Ledger { get; set; }
+
         public Tower()
         {
             this.Floors = new List<Floor>();
+            this.Ledger = new Ledger();
         }
 
-
+        private Ledger UpdateLedger()
+        {
+            foreach(var floor in this.Floors)
+            {
+                this.Ledger.TotalProfit += floor.Ledger.TotalProfit;
+                this.Ledger.TotalCost += floor.Ledger.TotalCost;
+            }
+            return Ledger;
+        }
     }
 }
