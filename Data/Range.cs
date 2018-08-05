738371b7fe14b0f4e45bd33f20ce85df3ee144dc
Kia Colbert <colber16@gmail.com>
2018-08-04 21:27:17 -0400

    Started getting rid of position in favor of range.

---
+++ Data/Range.cs
@@ -0,0 +1,16 @@
+﻿
+namespace KimTower.Data
+{
+    public struct Range
+    {
+        public int StartX { get; set;}
+
+        public int EndX { get; set; }
+
+        public Range(int startX, int endX)
+        {
+            this.StartX = startX;
+            this.EndX = endX;
+        }
+    }
+}
