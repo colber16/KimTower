232b5ef801a33eefa9b615d63a82ca88b32fdf8c
Kia Colbert <colber16@gmail.com>
2018-08-10 10:58:59 -0400

    Created RoomInfo for dictionary.

---
+++ Data/Rooms/StructureInfo.cs
@@ -0,0 +1,20 @@
+﻿
+namespace KimTower.Data.Rooms
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class RoomInfo
+    {
+       
+        public static Dictionary<StructureTypes, int> structureSegments = new Dictionary<StructureTypes, int>
+        {
+            {StructureTypes.Lobby, 4},
+            {StructureTypes.Office, 6},
+            {StructureTypes.StairCase, 6},
+            {StructureTypes.Restaurant, 10},
+            {StructureTypes.Condo, 8}
+
+        };
+    }
+}
